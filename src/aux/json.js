3e730f5ebfa193be29c976ffe9c5e308ebc62934
Edwinleon95 <ridergaleon@yahoo.com>
2022-05-27 20:49:11 -0500

    Bug Modal

--- src/aux/json.js
+++ src/aux/json.js
@@ -3,15 +3,15 @@ export const contentCardPlans = [{
     id: 1,
     title:'Principiante',
     description:'10 horas',
-    text:'Inscribe a tu amigo a este plan de 10 horas de paseo y recibirás 10% de descuento en el valor de la hora, aplica para paseo grupal y personalizado.'
+    text:'Inscribe a tu amigo a este plan de 10 horas de paseo y recibirás 10% de descuento sobre el valor de la hora, aplica para paseo grupal y personalizado.'
 }, {
     id: 2,
     title:'Intermedio',
     description:'15 horas',
-    text:'Inscribe a tu amigo a este plan de 15 horas de paseo y recibirás 20% de descuento en el valor de la hora, aplica para paseo grupal y personalizado.'
+    text:'Inscribe a tu amigo a este plan de 15 horas de paseo y recibirás 20% de descuento sobre el valor de la hora, aplica para paseo grupal y personalizado.'
 }, {
     id: 3,
     title:'Experto',
     description:'20 horas',
-    text:'Inscribe a tu amigo a este plan de 20 horas de paseo y recibirás 30% de descuento en el valor de la hora, aplica para paseo grupal y personalizado.'
+    text:'Inscribe a tu amigo a este plan de 20 horas de paseo y recibirás 30% de descuento sobre el valor de la hora, aplica para paseo grupal y personalizado.'
 }];
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,31 +1,36 @@
 import { bg_primary, bg_secondary, bg_section } from "../../aux/style"
 import { IconLogIn } from "../graphics/icons/LogIn"
 
-export const Modal = ({onClick,onClose}) => {
+export const Modal = ({ onClick, onClose }) => {
     return (
         <figure className="pt-3">
-            <div className="modal fade" id="exampleModalToggle" aria-labelledby="exampleModalToggleLabel" aria-hidden="true" style={bg_section}>
-                <div className="modal-dialog modal-dialog-centered">
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onClick={onClick}>
+                <IconLogIn px={'40px'} />
+            </button>
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true" style={bg_section}>
+                <div className="modal-dialog">
                     <div className="modal-content" style={bg_primary}>
                         <div className="modal-header text-white">
-                            <h5 className="modal-title fw-bold" id="exampleModalToggleLabel">Iniciar sesion</h5>
+                            <h5 className="modal-title" id="staticBackdropLabel">Iniciar sesion</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onClose}></button>
                         </div>
-                        <div className="modal-body">
-                            <div className="modal-body text-white">
-                                <h5>Usuario</h5>
-                                <input type="email" />
-                                <h5>Contraseña</h5>
-                                <input type="password" />
-                                <p> <a href="/" role="button" className="btn btn-secondary mt-2" data-bs-toggle="popover" title="Popover title" data-bs-content="Popover body content is set in this attribute." style={bg_secondary}>Iniciar sesion</a></p>
-                                <h5>¿No tienes cuenta?</h5>
-                                <a href="http://localhost:3000/" data-bs-toggle="tooltip" title="Tooltip">Registrarse</a>
-                            </div>
+                        <div className="modal-body text-white">
+                            <h5>Usuario</h5>
+                            <input type="email" />
+                            <h5>Contraseña</h5>
+                            <input type="password" />
+                            <p> <a href="/" role="button" className="btn btn-secondary mt-2" data-bs-toggle="popover"
+                                title="Popover title" data-bs-content="Popover body content is set in this attribute." style={bg_secondary}>Iniciar sesion</a></p>
+                            <h5>¿No tienes cuenta?</h5>
+                            <a href="/" data-bs-toggle="tooltip" title="Tooltip">Registrarse</a>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={onClose}>Close</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <a className="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button" onClick={onClick}><IconLogIn px={'30px'} /></a>
         </figure>
     )
 }
